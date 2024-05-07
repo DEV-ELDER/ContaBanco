187f33cbad6a1c0e25b263de52e7229776bfc62b
dev_elder <elder.santanadesouza@gmail.com>
2024-05-07 00:33:06 -0300

    projeto conta banco DIO

--- src/ContaTerminal.java
+++ src/ContaTerminal.java
@@ -1,5 +1,21 @@
+import java.util.Scanner;
+
 public class ContaTerminal {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
+        Scanner scanner = new Scanner(System.in);
+        
+        System.out.println("Digite o número da sua conta:");
+        int numeroConta = scanner.nextInt();
+
+        System.out.println("Digite o número da sua Agência:");
+        String agencia = scanner.next();
+
+        System.out.println("Digite seu nome:");
+        String nome = scanner.next();
+
+        double saldo = 237.48;
+        
+        
+        System.out.println("Olá".concat(nome));
     }
 }
